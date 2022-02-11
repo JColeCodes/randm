556775e22efd3e88ce77007da387fc05d6cff63e
Sparkle Party <lexi.slovik@gmail.com>
2022-02-11 11:42:26 -0800

    pull from develop

--- models/User.js
+++ models/User.js
@@ -3,65 +3,62 @@ const sequelize = require('../config/connection');
 
 class User extends Model {}
 
-User.init(
-  {
+User.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     first_name: {
-      type: DataTypes.STRING,
-      allowNUll: false,
+        type: DataTypes.STRING,
+        allowNUll: false,
     },
     last_name: {
-      type: DataTypes.STRING,
-      allowNUll: false,
+        type: DataTypes.STRING,
+        allowNUll: false,
     },
     email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8],
+        },
     },
     bio: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     gender: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     sexual_preference: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     pronouns: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     birthday: {
-      type: DataTypes.DATE,
-      allowNull: false,
+        type: DataTypes.DATE,
+        allowNull: false,
     },
-  },
-  {
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'user',
-  }
-);
+});
 
 module.exports = User;
