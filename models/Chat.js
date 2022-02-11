f1548327118d0a1d9d8f16efc09e5239e1316d3d
Sparkle Party <lexi.slovik@gmail.com>
2022-02-11 12:06:14 -0800

    working on index.js to test routes

--- models/Chat.js
+++ models/Chat.js
@@ -3,37 +3,34 @@ const sequelize = require('../config/connection');
 
 class Chat extends Model {}
 
-Chat.init(
-  {
+Chat.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     sender_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
     },
     receiver_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
     },
-  },
-  {
+}, {
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: 'chat',
-  }
-);
+});
 
 module.exports = Chat;
--- models/index.js
+++ models/index.js
@@ -2,21 +2,36 @@ const User = require('./User');
 const Message = require('./Message');
 const Chat = require('./Chat');
 
-User.hasMany(Chat, { foreignKey: 'sender_id' });
-User.hasMany(Chat, { foreignKey: 'receiver_id' });
-Chat.belongsToMany(User, { foreignKey: 'sender_id' });
-Chat.belongsToMany(User, { foreignKey: 'receiver_id' });
-
-Message.belongsToMany(User, {
-  through: Chat,
-  as: 'chat_messages',
-  foreignKey: 'user_id',
+User.hasMany(Chat, {
+    foreignKey: 'chat_id'
 });
 
-Chat.belongsToMany(User, {
-  through: Chat,
-  as: 'chat_messages',
-  foreignKey: 'receiver_id',
+Chat.belongsTo(User, {
+    foreignKey: 'sender_id'
+});
+
+Chat.belongsTo(User, {
+    foreignKey: 'receiver_id'
+});
+
+Message.hasMany(User, {
+    foreignKey: 'sender_id'
+});
+
+Message.hasMany(User, {
+    foreignKey: 'receiver_id'
+});
+
+Message.belongsToMany(User, {
+    through: Chat,
+    as: 'sent_messages',
+    foreignKey: 'sender_id',
+});
+
+Message.belongsToMany(User, {
+    through: Chat,
+    as: 'received_messages',
+    foreignKey: 'receiver_id',
 });
 
 module.exports = { User, Message };
