3ec879cf63ae14404cddf919019cc6dbd5e27edf
Charlie H <charltonhua@gmail.com>
2022-02-13 01:58:01 -0800

    update bulkCreate option, message seeds sender_id, add basic public js and handlebar files

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Message } = require('../models');
+const { Op } = require('sequelize');
 
 // GET all messages between two users, display in DESC order by created_at date/time
 router.get('/', (req, res) => {
   Message.findAll({
     where: {
-      // grab user id from the session data
-      receiver_id: req.session.user_id,
-      sender_id: req.session.user_id,
+      [Op.or]: [
+        // grab user id from the session data
+        { receiver_id: req.session.user_id },
+        { sender_id: req.session.user_id },
+      ],
     },
     attributes: ['id', 'message_text', 'created_at'],
     // display newest messages first
