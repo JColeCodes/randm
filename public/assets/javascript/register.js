1104a3f3c2aa597f9a3d53e92358a362aa5d4872
Charlie H <charltonhua@gmail.com>
2022-02-15 21:36:35 -0800

    fix login handlebars and js

---
+++ public/assets/javascript/register.js
@@ -0,0 +1,29 @@
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
