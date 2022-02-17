bc93d9eea4004326485e42ef88e38b411af5b48f
Sparkle Party <lexi.slovik@gmail.com>
2022-02-16 21:34:25 -0800

    add logout.js, update btn id

--- public/assets/javascript/logout.js
+++ public/assets/javascript/logout.js
@@ -11,4 +11,4 @@ async function logout() {
     }
 }
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('#logout-btn').addEventListener('click', logout);
