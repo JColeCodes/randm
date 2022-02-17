0993a64583a05b96cd335c8b88291fec158a462d
Sparkle Party <lexi.slovik@gmail.com>
2022-02-16 21:25:39 -0800

    merge with buttons branch

---
+++ public/assets/javascript/logout.js
@@ -0,0 +1,14 @@
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
