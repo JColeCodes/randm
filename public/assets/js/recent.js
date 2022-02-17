673f5d6a27a1e424e5e8ed80994ee4bcb6156032
Jennifer Cole <capauldi@gmail.com>
2022-02-16 21:48:34 -0800

    more cosmetic stuff

--- public/assets/js/recent.js
+++ public/assets/js/recent.js
@@ -7,18 +7,21 @@ function displayRecentChat() {
             const pageUrl = document.location.href.split('/');
             data.forEach(element => {
                 let recentLi = document.createElement("li");
+                let recentLink = document.createElement('a');
+                recentLink.setAttribute('href', `/chat/${element.id}`);
+
                 if (element.id == pageUrl[pageUrl.length - 1]){
                     recentLi.className = "selected";
                 }
-                recentLi.innerHTML = `<a href="/chat/${element.id}">
+                recentLi.innerHTML = `
                 <h3 class="name"> ${element.first_name} ${element.last_name}</h3>
-                <span class="latest-message">${element.latestMessage}</span>
-                </a>`;
+                <span class="latest-message">${element.latestMessage}</span>`;
                 
-                recentList.appendChild(recentLi);
+                recentLink.appendChild(recentLi);
+                recentList.appendChild(recentLink);
             });
         })
-        .catch(response => alert(response.statusText));
+        .catch(response => document.location.reload());
 }
 
 displayRecentChat();
