450993770fff2507985398923275b12f129a5968
Jennifer Cole <capauldi@gmail.com>
2022-02-17 04:05:51 -0800

    adding in req.session.user_id and adding redirects for unlogged users

--- public/assets/js/recent.js
+++ public/assets/js/recent.js
@@ -4,22 +4,26 @@ function displayRecentChat() {
     fetch('/api/messages/recent', { method: 'GET' })
         .then(response => response.json())
         .then(data => {
-            const pageUrl = document.location.href.split('/');
-            data.forEach(element => {
-                let recentLi = document.createElement("li");
-                let recentLink = document.createElement('a');
-                recentLink.setAttribute('href', `/chat/${element.id}`);
+            console.log(data);
+            if (data.length > 0) {
+                const pageUrl = document.location.href.split('/');
+                data.forEach(element => {
+                    let recentLi = document.createElement('li');
+                    let recentLink = document.createElement('a');
+                    recentLink.setAttribute('href', `/chat/${element.id}`);
 
-                if (element.id == pageUrl[pageUrl.length - 1]){
-                    recentLi.className = "selected";
-                }
-                recentLi.innerHTML = `
-                <h3 class="name"> ${element.first_name} ${element.last_name}</h3>
-                <span class="latest-message">${element.latestMessage}</span>`;
-                
-                recentLink.appendChild(recentLi);
-                recentList.appendChild(recentLink);
-            });
+                    if (element.id == pageUrl[pageUrl.length - 1]){
+                        recentLi.className = "selected";
+                    }
+                    recentLi.innerHTML = `
+                    <h3 class="name"> ${element.first_name} ${element.last_name}</h3>
+                    <span class="latest-message">${element.latestMessage}</span>`;
+                    
+                    recentLink.appendChild(recentLi);
+                    recentList.appendChild(recentLink);
+                });
+            }
+            recentList.innerHTML = `<li>You currently have no chats.</li>`;
         })
         .catch(response => document.location.reload());
 }
