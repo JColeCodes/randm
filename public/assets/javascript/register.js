5a0d176b4cf961edabea6821c4463881fa8b33d1
Charlie H <charltonhua@gmail.com>
2022-02-16 17:33:11 -0800

    calculate age setup

--- public/assets/javascript/register.js
+++ public/assets/javascript/register.js
@@ -1,29 +1,42 @@
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  // const username = document.querySelector('#username-signup').value.trim();
+  // const email = document.querySelector('#email-signup').value.trim();
+  // const password = document.querySelector('#password-signup').value.trim();
+  const birthday = document.querySelector('#birthday-register').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  const dob = Date.parse(birthday);
 
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+  function calculate_age(dob) {
+    var diff_ms = Date.now() - dob;
+    var age_dt = new Date(diff_ms);
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
   }
+
+  console.log(calculate_age(dob));
+  // var age = calculate_age(new Date(BIRTHYEAR, BIRTHMONTH-1, BIRTHDATE));
+
+  // if (username && email && password) {
+  //   const response = await fetch('/api/users', {
+  //     method: 'post',
+  //     body: JSON.stringify({
+  //       username,
+  //       email,
+  //       password,
+  //     }),
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
+
+  //   if (response.ok) {
+  //     document.location.replace('/');
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
 }
 
 document
-  .querySelector('.signup-form')
+  .querySelector('.register-form')
   .addEventListener('submit', signupFormHandler);
