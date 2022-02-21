85c39f5dd8744675decb6536cf5a064bcf38111d
Charlie H <charltonhua@gmail.com>
2022-02-20 18:04:04 -0800

    turned off all c.log on backend, remove sensitive user info, ordered messages by createdat

--- public/assets/js/message.js
+++ public/assets/js/message.js
@@ -1,38 +1,39 @@
 async function sendMessageFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const message_text = document.querySelector('#message').value.trim();
-    // get the id of the logged in user in the data-user attribute
-    const sender_id = document
-        .querySelector('#send-message-btn')
-        .getAttribute('data-user');
-    console.log(sender_id);
-    // grab the end of the URL and assign to receiver_id
-    const receiver_id = parseInt(window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ]);
+  const message_text = document.querySelector('#message').value.trim();
+  // get the id of the logged in user in the data-user attribute
+  const sender_id = document
+    .querySelector('#send-message-btn')
+    .getAttribute('data-user');
+  // console.log(sender_id);
+  // grab the end of the URL and assign to receiver_id
+  const receiver_id = parseInt(
+    window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ]
+  );
 
-    if (message_text) {
-        const response = await fetch('/api/messages', {
-            method: 'POST',
-            body: JSON.stringify({
-                sender_id,
-                receiver_id,
-                message_text,
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+  if (message_text) {
+    const response = await fetch('/api/messages', {
+      method: 'POST',
+      body: JSON.stringify({
+        sender_id,
+        receiver_id,
+        message_text,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-        if (response.ok) {
-            // reload current page so new message will be added to thread
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      // reload current page so new message will be added to thread
+      document.location.reload();
+    } else {
+      alert(response.statusText);
     }
-
+  }
 }
 
 document
-    .querySelector('.send-message')
-    .addEventListener('submit', sendMessageFormHandler);
+  .querySelector('.send-message')
+  .addEventListener('submit', sendMessageFormHandler);
